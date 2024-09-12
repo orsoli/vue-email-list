d76aa93a8ecd18add61f60a71e49936198c670e7
orsoli <fljcoli@gmail.com>
2024-09-12 12:28:18 +0200

    Added print on page 10 randon emails step

--- js/index.js
+++ js/index.js
@@ -5,7 +5,28 @@ createApp({
   // Create variables
   data() {
     return {
-      message: "Hello Vue!",
+      apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail", // Variable to store url
+      emails: [], // Variable to store random emails
     };
+  },
+
+  methods: {
+    // Define a function to get response from bollean url by API response
+    getRandomEmail() {
+      // Generate 10 diferent random email
+      for (let i = 0; i < 10; i++) {
+        // Get response from url
+        axios.get(this.apiUrl).then((response) => {
+          // Add random email gererated in emails array
+          this.emails.push(response.data.response);
+        });
+      }
+    },
+  },
+
+  mounted() {
+    // Call back function
+    this.getRandomEmail();
+    console.log(this.emails); // Testing orint in console
   },
 }).mount("#app");
